a5a22e790230e87d5bfe77ce6f924c54261028a3
gilney nathanael mathias <gilney_salvo@hotmail.com>
2017-09-10 13:41:42 -0300

    Inicio da internacionalização do programa

--- Leilao/src/main/java/exceptions/UsuarioNaoCadastradoException.java
+++ Leilao/src/main/java/exceptions/UsuarioNaoCadastradoException.java
@@ -1,10 +1,12 @@
 package exceptions;
 
+import util.I18n;
+
 public class UsuarioNaoCadastradoException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 	
 	public UsuarioNaoCadastradoException() {
-		super("O usuario nao esta cadastrado.");
+		super(I18n.getInstance().getString("exceptions.usuario_nao_cadastrado"));
 	}
 }
---
+++ Leilao/src/main/java/util/I18n.java
@@ -0,0 +1,70 @@
+package util;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class I18n {
+	
+	private static final String BUNDLE_PREFIX = "MessagesBundle";
+	private static final String TRANSLATIONS_FOLDER = "./translations";
+	
+	private static I18n instance;
+	
+	private ResourceBundle bundle;
+	private HashMap<String,Locale> locales;
+	
+	private I18n() {
+		this.locales = new HashMap<>();
+		
+		this.carregaBundle(Locale.getDefault());
+		this.encontraTraducoesDisponiveis();
+	}
+	
+	public static I18n getInstance() {
+		if(instance != null)
+			return instance;
+		return new I18n();
+	}
+	
+	public String getString(String key) {
+		return this.bundle.getString(key);
+	}
+	
+	public SortedSet<String> getLocalesOrdenados(){
+		return new TreeSet<>(this.locales.keySet());
+	}
+	
+	public void carregaBundle(Locale locale) {
+		try {
+			File file = new File(TRANSLATIONS_FOLDER);
+			URL[] urls = {file.toURI().toURL()};
+			ClassLoader loader = new URLClassLoader(urls);
+			this.bundle = ResourceBundle.getBundle(BUNDLE_PREFIX, locale, loader);			
+		}catch(MalformedURLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void encontraTraducoesDisponiveis() {
+		File[] files = new File(TRANSLATIONS_FOLDER).listFiles();
+		String name, key;
+		String[] tmp;
+		for (File file : files) {
+		    if (file.isFile()) {
+		        name = file.getName();
+		        if(name.contains("_")) {
+		        	key = name.substring(name.indexOf('_')+1, name.indexOf('.'));
+		        	tmp = key.split("_");
+		        	this.locales.put(key, new Locale(tmp[0], tmp[1]));
+		        }
+		    }
+		}
+	}
+}
