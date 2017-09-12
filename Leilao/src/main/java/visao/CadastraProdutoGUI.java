31738d20983aa8d10dfcd6629c81f836d5e42f73
gilney nathanael mathias <gilney_salvo@hotmail.com>
2017-09-12 09:49:19 -0300

    Adição das traduções da janela de Cadastrar Prods
    Além da adição de mais alguns traduções de exceções que foram esquecidas

--- Leilao/src/main/java/visao/CadastraProdutoGUI.java
+++ Leilao/src/main/java/visao/CadastraProdutoGUI.java
@@ -24,47 +24,47 @@ public class CadastraProdutoGUI extends ParentGUI {
 	
 	@Override
 	protected void constroiFrame(final PrincipalGUI parent, final MercadoLeilao mercado) {
-		currentFrame.setTitle("Cadastrar Produto");
+		currentFrame.setTitle(i18n.getString("cadastraProdutoGUI.titulo"));
 		currentFrame.setSize(450, 423);
 		currentFrame.getContentPane().setLayout(new MigLayout("al center center,fillx", 
 				"[grow]", "[][]20[][]20[][]20[][]20[][]30[]"));
 		
-		final JLabel lblNome = new JLabel("Nome produto:");
+		final JLabel lblNome = new JLabel(i18n.getString("cadastraProdutoGUI.nome_produto"));
 		currentFrame.getContentPane().add(lblNome, "span,grow");
 		
 		final JTextField tfNome = new JTextField();
 		currentFrame.getContentPane().add(tfNome, "span,grow,height 25::");
 		tfNome.setColumns(10);
 		
-		final JLabel lblDescricao = new JLabel("Descrição do produto:");
+		final JLabel lblDescricao = new JLabel(i18n.getString("cadastraProdutoGUI.descricao_produto"));
 		currentFrame.getContentPane().add(lblDescricao, "span,grow");
 		
 		final JTextField tfDescricao = new JTextField();
 		currentFrame.getContentPane().add(tfDescricao, "span,grow,height 25::");
 		tfDescricao.setColumns(10);
 		
-		final JLabel lblLanceMin = new JLabel("Lance mínimo:");
+		final JLabel lblLanceMin = new JLabel(i18n.getString("cadastraProdutoGUI.lance_minimo"));
 		currentFrame.getContentPane().add(lblLanceMin, "span,grow");
 		
 		final JTextField tfLanceMin = new JTextField();
 		currentFrame.getContentPane().add(tfLanceMin, "span,grow,height 25::");
 		tfLanceMin.setColumns(10);
 		
-		final JLabel lblApelidoLeiloador = new JLabel("Apelido do leiloador:");
+		final JLabel lblApelidoLeiloador = new JLabel(i18n.getString("cadastraProdutoGUI.apelido_leiloador"));
 		currentFrame.getContentPane().add(lblApelidoLeiloador, "span,grow");
 		
 		final JTextField tfApelidoLeiloador = new JTextField();
 		currentFrame.getContentPane().add(tfApelidoLeiloador, "span,grow,height 25::");
 		tfApelidoLeiloador.setColumns(10);
 		
-		final JLabel lblDataLimite = new JLabel("Data limite:");
+		final JLabel lblDataLimite = new JLabel(i18n.getString("cadastraProdutoGUI.data_limite"));
 		currentFrame.getContentPane().add(lblDataLimite, "span,grow");
 		
 		final JTextField tfDataLimite = new JTextField();
 		currentFrame.getContentPane().add(tfDataLimite, "span,grow,height 25::");
 		tfDataLimite.setColumns(10);
 		
-		final JButton btnCadastrar = new JButton("Cadastrar");
+		final JButton btnCadastrar = new JButton(i18n.getString("cadastraProdutoGUI.cadastrar"));
 		btnCadastrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				String nome = tfNome.getText();
@@ -79,11 +79,11 @@ public class CadastraProdutoGUI extends ParentGUI {
 					mercado.cadastrarProduto(nome, descricao, valorMinimo, apelidoLeiloador, dataLimite);
 					currentFrame.dispose();
 				} catch (NumberFormatException e) {
-					parent.mostraMensagemDeAlerta(currentFrame, "O valor do lance mínimo deve ser um numero.");
-					System.err.println(e.getMessage());
+					parent.mostraMensagemDeAlerta(currentFrame, i18n.getString("exceptions.lance_min_numero"));
+					System.err.println("CadastraProdutoGUI:> " +e.getMessage());
 				} catch (ParseException e) {
-					parent.mostraMensagemDeAlerta(currentFrame, "Data limite invalida.");
-					System.err.println(e.getMessage());
+					parent.mostraMensagemDeAlerta(currentFrame, i18n.getString("exceptions.data_limite_invalida"));
+					System.err.println("CadastraProdutoGUI:> " +e.getMessage());
 				} catch (CadastroProdutoException e) {
 					parent.mostraMensagemDeAlerta(currentFrame, e.getMessage());
 				} 
